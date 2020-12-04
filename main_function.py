be006795e7c04fca7ee482d319b50f0f1c2528a0
kerembyl <you@example.com>
2020-12-04 21:29:44 +0300

    msg objesi yaratıldı, sendmail yerine send_message metodu kullanıldı

--- main_function.py
+++ main_function.py
@@ -1,27 +1,29 @@
-import smtplib, ssl
+import smtplib
+import ssl
+from email.utils import formataddr
+from email.message import EmailMessage
 
+# Server, sender and receiver information
 smtp_server = "smtp.gmail.com"
 port = 587 # For starttls
 sender_email = "developmentkerem@gmail.com"
-password = input(f"Enter your password for {sender_email}:")
-
+sender_password = input(f"Enter your password for {sender_email}:")
 receiver_email = "kerem_boyali@hotmail.com"
-message = """\
-Subject: Hi there
+context = ssl.create_default_context() # Create a secure SSL context
 
-This message is sent from Python."""
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+# Construct the message object
+msg = EmailMessage()
+msg['From'] = formataddr(('Example Sender Name', sender_email))
+msg['To'] = formataddr((receiver_email, receiver_email))
+msg.set_content('Lorem ipsum')
 
 # Try to log in to server and send email
 try:
     server = smtplib.SMTP(smtp_server, port)
-    server.ehlo()
     server.starttls(context=context) # Secure the connection
-    server.ehlo()
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message) # Send e-mail
+    server.login(sender_email, sender_password)
+    server.send_message(msg) # Send e-mail
 except Exception as e:
     # Print any error messages to stdout
     print(e)
