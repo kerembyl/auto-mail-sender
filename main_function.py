f12d02b2eaf75050108f69598d3af4e41ed83327
kerembyl <you@example.com>
2020-12-04 21:45:08 +0300

    Kod sadeleştirildi ve Subject eklendi

--- main_function.py
+++ main_function.py
@@ -6,26 +6,28 @@ from email.message import EmailMessage
 # Server, sender and receiver information
 smtp_server = "smtp.gmail.com"
 port = 587 # For starttls
+context = ssl.create_default_context() # Create a secure SSL context
 sender_email = "developmentkerem@gmail.com"
+sender_name = "Her gün 1 kelime"
 sender_password = input(f"Enter your password for {sender_email}:")
 receiver_email = "kerem_boyali@hotmail.com"
-context = ssl.create_default_context() # Create a secure SSL context
-
+receiver_name = receiver_email
 
 # Construct the message object
 msg = EmailMessage()
-msg['From'] = formataddr(('Example Sender Name', sender_email))
-msg['To'] = formataddr((receiver_email, receiver_email))
-msg.set_content('Lorem ipsum')
+msg['From'] = formataddr((sender_name, sender_email))
+msg['To'] = formataddr((receiver_name, receiver_email))
+msg['Subject'] = "Bugünün kelimesi: Deneme"
+msg.set_content("Lorem ipsum")
 
 # Try to log in to server and send email
 try:
-    server = smtplib.SMTP(smtp_server, port)
-    server.starttls(context=context) # Secure the connection
-    server.login(sender_email, sender_password)
-    server.send_message(msg) # Send e-mail
+    s = smtplib.SMTP(smtp_server, port)
+    s.starttls(context=context) # Secure the connection
+    s.login(sender_email, sender_password)
+    s.send_message(msg) # Send e-mail
 except Exception as e:
     # Print any error messages to stdout
     print(e)
 finally:
-    server.quit()
+    s.quit()
